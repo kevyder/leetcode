5a99669e6be9951620081674591d6e38dc954a6d
Kevin Rodriguez <kyrodriguez97@gmail.com>
2023-10-25 14:20:51 -0500

    Longest Common Prefix

---
+++ longest_common_prefix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        prefix = ""
+        strs = list(set(strs))
+        first_string = strs[0]
+
+        if len(strs) == 1:
+            return first_string
+
+        for i in range(1, len(first_string) + 1):
+            sliced_elements = slice(i)
+            for s in strs:
+                if first_string[sliced_elements] != s[sliced_elements]:
+                    return prefix
+            prefix = first_string[sliced_elements]
+        return prefix
