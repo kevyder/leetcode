f13811d8b139f6622807fc681d04fe29e7088b33
Kevin Rodriguez <kyrodriguez97@gmail.com>
2023-11-27 18:14:57 -0500

    Invert Binary Tree

---
+++ invert_binary_tree.py
@@ -0,0 +1,24 @@
+# 226. Invert Binary Tree
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        def dfs(node):
+            if not node:
+                return
+
+            dfs(node.left)
+            dfs(node.right)
+
+            node.left, node.right = node.right, node.left
+
+        dfs(root)
+
+        return root
